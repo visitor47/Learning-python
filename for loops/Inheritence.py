dad0395263fa59cce1f502150ce83f91c4d2d541
Visitor47 <ceglover91@gmail.com>
2018-09-26 20:42:15 -0600

    3 new files showing inheritance (chef, chinese chef, and inheritence)

---
+++ for loops/Inheritence.py
@@ -0,0 +1,13 @@
+from chefClass import chef
+from ChineseChef import ChineseChef
+
+
+myChef = chef()
+
+myChef.make_chicken()
+myChef.make_soup()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_fried_rice()
+myChineseChef.make_soup()
+myChineseChef.make_chicken()
